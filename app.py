95ddf385fd9f6a4c94b3d49ed733c9b282a4564e
Rogendo <peterrogendo1999@gmail.com>
2023-12-16 19:55:03 +0300

    Translator and language recognizer

--- app.py
+++ app.py
@@ -10,6 +10,64 @@ from keras.models import load_model
 model = load_model('model.h5')
 import json
 import random
+
+
+
+
+from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
+import spacy
+from spacy.language import Language
+from spacy_langdetect import LanguageDetector
+
+
+# translator pipeline for english to swahili translations
+eng_swa_model_checkpoint = "Helsinki-NLP/opus-mt-en-swc"
+eng_swa_tokenizer = AutoTokenizer.from_pretrained("model/eng_swa_model/")
+eng_swa_model = AutoModelForSeq2SeqLM.from_pretrained("model/eng_swa_model/")
+
+eng_swa_translator = pipeline(
+    "text2text-generation",
+    model=eng_swa_model,
+    tokenizer=eng_swa_tokenizer,
+)
+
+def translate_text_eng_swa(text):
+    translated_text = eng_swa_translator(text, max_length=128, num_beams=5)[0]['generated_text']
+    return translated_text
+
+# translator pipeline for swahili to english translations
+swa_eng_model_checkpoint = "Helsinki-NLP/opus-mt-swc-en"
+swa_eng_tokenizer = AutoTokenizer.from_pretrained("model/swa_eng_model/")
+swa_eng_model = AutoModelForSeq2SeqLM.from_pretrained("model/swa_eng_model/")
+
+swa_eng_translator = pipeline(
+    "text2text-generation",
+    model=swa_eng_model,
+    tokenizer=swa_eng_tokenizer,
+)
+
+def translate_text_swa_eng(text):
+    translated_text = swa_eng_translator(text, max_length=128, num_beams=5)[0]['generated_text']
+    return translated_text
+
+
+# Language detector
+def get_lang_detector(nlp, name):
+    return LanguageDetector()
+
+# # Load the English language model
+nlp = spacy.load("en_core_web_sm")
+
+# # Register the language detection factory
+Language.factory("language_detector", func=get_lang_detector)
+
+# # Add the language detection component to the pipeline
+nlp.add_pipe('language_detector', last=True)
+
+
+
+
+
 intents = json.loads(open('intents.json').read())
 words = pickle.load(open('texts.pkl','rb'))
 classes = pickle.load(open('labels.pkl','rb'))
@@ -53,10 +111,24 @@ def getResponse(ints, intents_json):
             result = random.choice(i['responses'])
             break
     return result
+
 def chatbot_response(msg):
     ints = predict_class(msg, model)
     res = getResponse(ints, intents)
+    print(res)
+    # language detection
+    doc=nlp(res)
+    detected_language = doc._.language['language']
+    print(f"Detected language: {detected_language}")   
+
+    if detected_language=="en":   
+        print(translate_text_eng_swa(res))
+    elif detected_language=='sw':
+        print(translate_text_swa_eng(res))
+
     return res
+    
+# Creating GUI with flask
 from flask import Flask, render_template, request
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -66,6 +138,7 @@ def home():
 @app.route("/get")
 def get_bot_response():
     userText = request.args.get('msg')
+    print(userText)
     return chatbot_response(userText)
 if __name__ == "__main__":
     app.run()
